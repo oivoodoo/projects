cf0351d6a24bb7716525a97c61e28ce25390ca9f
korsaks <korsaks@korsaks.(none)>
2011-03-03 11:51:51 +0200

    updates

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :repository,  "git@github.com:oivoodoo/projects.git"
 
 default_run_options[:pty] = true
 set :ssh_options, { :forward_agent => true }
-set :user, "hosting_nosimoe"
+set :user, "hosting_onfly"
 
 role :web, "lithium.locum.ru"                          # Your HTTP server, Apache/etc
 role :app, "lithium.locum.ru"                          # This may be the same as your `Web` server
@@ -24,3 +24,4 @@ namespace :deploy do
     task t, :roles => :app do ; end
   end
 end
+
